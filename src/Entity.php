5a52cfee282dc1a324e322889b4ad2a99526d25f
Sergey Populov <sergey@saritasa.com>
2017-04-12 15:57:47 +0500

    Add enums support to Entity

--- src/Entity.php
+++ src/Entity.php
@@ -3,7 +3,13 @@
 namespace Saritasa\Database\Eloquent;
 
 use Illuminate\Database\Eloquent\Model;
+use Saritasa\Enum;
 
+/**
+ * Adds some features to Eloquent model:
+ * Enums cast. See TODO: Doc URL
+ * Ability to define default values. See TODO: Doc URL
+ */
 class Entity extends Model
 {
     const ID = 'id';
@@ -12,9 +18,63 @@ class Entity extends Model
 
     protected $defaults = [];
 
+    /**
+     * The attributes that should be cast to Enum classes
+     * @var array
+     */
+    protected $enums = [];
+
     public function __construct(array $attributes = [])
     {
         $this->setRawAttributes($this->defaults, true);
         parent::__construct($attributes);
+    }
+
+    /**
+     * Gets the value of attribute
+     * @param string $key
+     * @return mixed
+     */
+    public function getAttributeValue($key)
+    {
+        $value = $this->getAttributeFromArray($key);
+        // check if it's a enum
+        if (isset($this->enums[$key]) && is_scalar($value)) {
+            $enumClass = $this->enums[$key];
+            return new $enumClass($value);
+        }
+        return parent::getAttributeValue($key);
+    }
+
+    /**
+     * Sets the value of attribute
+     * @param string $key
+     * @param mixed $value
+     * @return $this
+     */
+    public function setAttribute($key, $value)
+    {
+        // check if it's a enum
+        if (isset($this->enums[$key]) && is_scalar($value)) {
+            $enumClass = $this->enums[$key];
+            $this->attributes[$key] = new $enumClass($value);
+        } else {
+            parent::setAttribute($key, $value);
+        }
+        return $this;
+    }
+
+    /**
+     * Gets validation rule for given enum class
+     * @param string $enumClass
+     * @return array
+     */
+    protected static function getEnumValidationRule(string $enumClass) : array
+    {
+        if (!is_a($enumClass, Enum::class, true)) {
+            throw new \UnexpectedValueException('Class is not enum');
+        }
+
+        return array_merge(['in'], $enumClass::getConstants());
     }
 }
